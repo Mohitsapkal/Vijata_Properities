182b03e900874c99054867bd4dcdefef559450ec
Mohit sapkal <mohitsapkal41@gmail.com>
2026-07-02 17:36:03 +0530

    commits

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { MessageCircle } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { projects } from "@/data/projects";
 
@@ -36,7 +35,14 @@ export default function WhatsAppButton() {
       <span className="absolute inline-flex h-14 w-14 animate-ping rounded-full bg-[#25D366] opacity-40 delay-300"></span>
       
       {/* WhatsApp Icon */}
-      <MessageCircle className="relative h-8 w-8 transition-transform duration-300 group-hover:rotate-12" fill="currentColor" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="relative h-9 w-9 transition-transform duration-300 group-hover:rotate-12"
+      >
+        <path d="M12.031 0C5.385 0 0 5.385 0 12.031c0 2.128.555 4.195 1.613 6.012L.416 23.584l5.688-1.492A11.968 11.968 0 0012.031 24c6.646 0 12.031-5.385 12.031-12.031S18.677 0 12.031 0zm6.541 17.202c-.276.776-1.583 1.488-2.186 1.545-.561.053-1.282.261-4.086-.902-3.385-1.405-5.59-4.887-5.759-5.113-.17-.226-1.375-1.833-1.375-3.493 0-1.66.862-2.476 1.171-2.822.31-.345.674-.431.899-.431.226 0 .452 0 .647.011.206.011.481-.077.751.57.283.676.966 2.361 1.05 2.531.085.17.142.368.028.594-.113.226-.17.368-.34.566-.17.198-.354.439-.509.58-.17.155-.353.325-.155.666.198.341.879 1.455 1.888 2.355 1.3 1.161 2.388 1.521 2.727 1.691.34.17.538.141.74-.085.198-.226.852-.995 1.079-1.336.226-.341.452-.283.765-.17.311.113 1.966.927 2.305 1.097.339.17.565.254.649.395.085.141.085.819-.191 1.595z" />
+      </svg>
     </a>
   );
 }
